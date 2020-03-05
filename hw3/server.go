ff676b3d06562442f6af99a7070a8cb53bbfc93a
Бобков Алексей Андреевич <11ap.abobkov@vas.arbitr.ru>
2020-03-05 15:27:09 +0400

    Правки

--- hw3/server.go
+++ hw3/server.go
@@ -8,16 +8,6 @@ import (
 	"strings"
 )
 
-var blog1 = Blog{
-	Name:        "Личный блог",
-	Description: "Посты на разные темы",
-	Posts: []Post{
-		{ID: 1, Header: "Первый пост", Text: "13241 3g r1344 23 3fdgdg "},
-		{ID: 2, Header: "Второй пост", Text: "hjghj 45456 435th hhth4 hth"},
-		{ID: 3, Header: "Третий Пост", Text: "th4th4 t4ht 4th th 4tht ht"},
-	},
-}
-
 var (
 	tmplBlog    = template.Must(template.New("BlogTemplate").ParseFiles("index.html"))
 	tmplPost    = template.Must(template.New("PostTemplate").ParseFiles("post.html"))
@@ -25,18 +15,27 @@ var (
 )
 
 func main() {
+	var blog1 = Blog{
+		Name:        "Личный блог",
+		Description: "Посты на разные темы",
+		Posts: []Post{
+			{ID: 1, Header: "Первый пост", Text: "13241 3g r1344 23 3fdgdg "},
+			{ID: 2, Header: "Второй пост", Text: "hjghj 45456 435th hhth4 hth"},
+			{ID: 3, Header: "Третий Пост", Text: "th4th4 t4ht 4th th 4tht ht"},
+		},
+	}
 	router := http.NewServeMux()
 	router.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("src"))))
-	router.HandleFunc("/blog", showBlog)
-	router.HandleFunc("/blog/newpost", newPost)
-	router.HandleFunc("/blog/", showPost)
+	router.HandleFunc("/blog", blog1.showBlog)
+	router.HandleFunc("/blog/newpost", blog1.newPost)
+	router.HandleFunc("/blog/", blog1.showPost)
 	port := "8080"
 	log.Printf("start listen on port %v", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
 
 }
 
-func showBlog(w http.ResponseWriter, r *http.Request) {
+func (blog1 *Blog) showBlog(w http.ResponseWriter, r *http.Request) {
 	if err := tmplBlog.ExecuteTemplate(w, "blog", blog1); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -44,7 +43,7 @@ func showBlog(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func showPost(w http.ResponseWriter, r *http.Request) {
+func (blog1 *Blog) showPost(w http.ResponseWriter, r *http.Request) {
 	path := strings.Split(r.URL.Path, "/")
 	p, err := strconv.Atoi(path[2])
 	if err != nil {
@@ -58,7 +57,7 @@ func showPost(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func newPost(w http.ResponseWriter, r *http.Request) {
+func (blog1 *Blog) newPost(w http.ResponseWriter, r *http.Request) {
 	lenBlog := len(blog1.Posts)
 	newpost := Post{
 		ID:     lenBlog + 1,
